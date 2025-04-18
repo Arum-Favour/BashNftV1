fa43250c86ecb02af21d002ddef1c6744d1ea069
Arum <nwajiudo@gmail.com>
2025-04-18 09:57:45 +0100

    created mood Nft Contract

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,24 +2,30 @@
 
 pragma solidity ^0.8.24;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
-   uint256 public tokenCounter;
-   string private s_sadSvg;
-   string private s_happySvg;
+   uint256 public s_tokenCounter;
+   string private s_sadSvgImageUri;
+   string private s_happySvgImageUri;
 
    // string public constant BASE_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=";
 
-    constructor( string memory sadSvg, string memory happySvg) ERC721("MoodNft", "MNFT") {
-        tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+    constructor( string memory sadSvgImageUri, string memory happySvgImageUri) ERC721("MoodNft", "MNFT") {
+        s_tokenCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
 
     function mintNft(string memory tokenUri) public {
-        _safeMint(msg.sender, tokenCounter);
-        _setTokenURI(tokenCounter, tokenUri);
-        tokenCounter++;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
+
+    function tokenURI(uint256 tokenId) public view override returns(string memory){
+        string memory tokenMetadata = string.concat('{"name": "',name(),"'}');
+    }
+
+
 }
